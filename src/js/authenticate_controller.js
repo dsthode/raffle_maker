466f62baecdf5e4a500f6aa1bd10b2751f0c7114
Damian Serrano Thode <dsthode@gmail.com>
2015-04-09 01:24:09 +0200

    Fixed Meetup oauth flow

--- src/js/authenticate_controller.js
+++ src/js/authenticate_controller.js
@@ -1,16 +1,12 @@
 'use strict';
 
 angular.module('raffle_maker')
-.controller('AuthenticationController', ['$window', 'CONFIG', function($window, CONFIG) {
+.controller('AuthenticationController', ['$scope', '$window', 'CONFIG', function($scope, $window, CONFIG) {
 	$scope.vars = {
 		api_key: ''
 	};
 
 	$scope.authenticate = function() {
-		if ($scope.vars.api_key.length > 0) {
-			$window.$location(CONFIG.AUTH_URL + '&client_id=' + $scope.vars.api_key);
-		} else {
-			alert('You must enter your Meetup API key');
-		}
+		$window.location.assign(CONFIG.AUTH_URL);
 	};
 }]);
